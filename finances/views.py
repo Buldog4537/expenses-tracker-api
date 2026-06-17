2a88366e16fd9e228f5a15aff6e34147450974bd
Kyler Norris <kylernorris@yahoo.com>
2026-06-16 19:30:44 -0700

    Added views file to finances

--- finances/views.py
+++ finances/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Income, Category, Expense, Budget
+from .serializers import (
+    IncomeSerializer,
+    CategorySerializer,
+    ExpenseSerializer,
+    BudgetSerializer,
+)
+
+
+class IncomeViewSet(ModelViewSet):
+    queryset = Income.objects.all()
+
+    serializer_class = IncomeSerializer
+
+    permission_classes = [IsAuthenticated]
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+
+    serializer_class = CategorySerializer
+
+    permission_classes = [IsAuthenticated]
+
+
+class ExpenseViewSet(ModelViewSet):
+    queryset = Expense.objects.all()
+
+    serializer_class = ExpenseSerializer
+
+    permission_classes = [IsAuthenticated]
+
+
+class BudgetViewSEt(ModelViewSet):
+    queryset = Budget.objects.all()
+
+    serializer_class = BudgetSerializer
+
+    permission_classes = [IsAuthenticated]
